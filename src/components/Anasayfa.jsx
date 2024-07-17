a0f24301b50cdbffbb6c6057fa80e9d6d7ceb3ed
Yusuf Kenan Girgin <yusufkenangirgin@hotmail.com>
2024-07-17 15:43:39 +0300

    deneme

---
+++ src/components/Anasayfa.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Anasayfa = () => {
+    let routerPic = "https://plus.unsplash.com/premium_photo-1682145723071-33e2207f4eb1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  return (
+    <div className='main'>
+        <h3>ANK-16 React Router</h3>
+        <img src={routerPic} alt="router" />
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ut eaque, quas dignissimos est distinctio quidem delectus ratione velit mollitia dolore nam asperiores suscipit obcaecati accusamus doloribus corporis nihil beatae quod praesentium impedit doloremque. At voluptas eaque expedita nulla sed labore eum veniam tenetur doloremque voluptatem soluta facilis corporis cum quisquam vero ipsum, eos animi harum. Iste quidem minima numquam sint dolorem corporis natus dolores, quisquam accusamus deleniti tenetur consectetur. Voluptatem accusamus ab sequi natus ut consequatur necessitatibus adipisci. Saepe optio temporibus ullam non error minus placeat amet at. Reiciendis, magnam fuga dolores tempora eligendi dolorem aliquam eum numquam facere repudiandae a maxime dicta tenetur, atque omnis. Quia reprehenderit ducimus laboriosam quibusdam veniam. Quia ipsum deleniti pariatur quibusdam error nostrum cumque veritatis, molestiae recusandae quaerat facere deserunt, temporibus, aliquam magnam minus. Neque, reiciendis! Incidunt, odio eum modi optio eos dolore excepturi accusantium illum nesciunt quis quibusdam reiciendis laudantium voluptates, enim sunt! Cumque quaerat reiciendis dolorum recusandae ad praesentium odit sequi molestias eum sunt similique pariatur harum esse doloremque omnis aliquid quod ut nesciunt laborum, consequuntur possimus, officia provident fugiat illum. Iure temporibus vitae eveniet provident nobis nemo sit recusandae eius explicabo, repellendus ab, nesciunt ea quae excepturi placeat. Distinctio, natus?
+        </p>
+    </div>
+  )
+}
+
+export default Anasayfa
